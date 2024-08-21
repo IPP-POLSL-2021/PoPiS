4f0c7c28f77726501a579f13a65158f58d7d62b0
kingbrucelee <90533878+kingbrucelee@users.noreply.github.com>
2024-08-22 01:05:35 +0200

    Version 2
    - Instancja Client jest teraz tworzona tam gdzie powinna czyli bezpośrednio w funkcji a nie przekazywana
    - Można teraz przekazać punkt startowy jako argument
    - Argumenty teraz mają swoją domyślną wartość  (wywołuje funkcje niższego szczebla lub hard-coded najnowsze dane na dzień pisania kodu)
    TODO:
    Oczywiście wymyślenie sposobu na zapis tych danych od razu odradzam zmienne środowiskowe.
    Chyba będzie trzeba bazę danych po prostu, im szybciej tym lepiej

--- current_number.py
+++ current_number.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 # Robi za nas user-agent, headery, cookies itp generalnie sprawia że API nas chce
 from api_for_polish_sejm_client import Client
 
@@ -16,14 +14,18 @@ from api_for_polish_sejm_client.api.default import get_sejm_termterm
 from api_for_polish_sejm_client.api.default import get_sejm_termterm_votings_sitting
 from api_for_polish_sejm_client.api.default import get_sejm_termterm_votings_sitting_num
 
+# By robić losowe opóźnienia. Niech ktoś się wypowie czy cringe czy dobry pomysł
 import random
+from time import sleep
 
 # Można by zrobić argumenty opcjonalnymi i wywoływać funkcje podległe 
+# Done
 
-def get_term_number(client): # 21.08.2024 - Ostatnia kadencja sejmu miała numer 10
-    with client as client:
+def get_term_number(current_term_number=10): # 21.08.2024 - Ostatnia kadencja sejmu miała numer 10
+    
+    with Client(base_url="https://api.sejm.gov.pl") as client:
         # 1 kadencja Sejmu też nie zwraca nic XDDD
-        erw=2
+        erw=current_term_number
         while (True):
             response: Response[Term] = get_sejm_termterm.sync_detailed(term=erw, client=client)
             if not response.parsed:
@@ -31,9 +33,9 @@ def get_term_number(client): # 21.08.2024 - Ostatnia kadencja sejmu miała numer
             erw+=1
             sleep(random.random()/10)
 
-def get_sitting_number(client,term): # 21.08.2024 - Ostatnie posiedzenie sejmu 10 kadencji miało numer 16
-    with client as client:
-        erw=1
+def get_sitting_number(term=get_term_number(), current_sitting_number=16): # 21.08.2024 - Ostatnie posiedzenie sejmu 10 kadencji miało numer 16
+    with Client(base_url="https://api.sejm.gov.pl") as client:
+        erw=current_sitting_number
         while(True):
             response: Response[Voting] = get_sejm_termterm_votings_sitting.sync_detailed(term=term, sitting=erw, client=client)
             if not response.parsed:
@@ -41,21 +43,19 @@ def get_sitting_number(client,term): # 21.08.2024 - Ostatnie posiedzenie sejmu 1
             erw+=1
             sleep(random.random()/10)
 
-def get_voting_number(client,term, sitting): # 21.08.2024 - Ostatnie głosowanie 16 posiedzenia sejmu 10 kadencji miało numer 85
-    with client as client: 
-        erw=1
+def get_voting_number(term=get_term_number(), sitting=get_sitting_number(), current_voting_number=85): # 21.08.2024 - Ostatnie głosowanie 16 posiedzenia sejmu 10 kadencji miało numer 85
+    with Client(base_url="https://api.sejm.gov.pl") as client: 
+        erw=current_voting_number
         while(True):
             response: Response[VotingDetails] = get_sejm_termterm_votings_sitting_num.sync_detailed(term=term, sitting=sitting, client=client, num=erw)
             if not response.parsed:
                 return erw-1
             erw+=1
             sleep(random.random()/100)
+            
+# Jeśli wywołujemy skrypt bezpośrednio
 if __name__ == "__main__" :
-    # Handler "with" zamyka Clienta którego nie da się ponownie otworzyć
-    client = Client(base_url="https://api.sejm.gov.pl")
-    term_number=get_term_number(client)
-    client = Client(base_url="https://api.sejm.gov.pl")
-    sitting_number=get_sitting_number(client,term_number)
-    client = Client(base_url="https://api.sejm.gov.pl")
-    voting_number=get_voting_number(client,term_number,sitting_number)
+    term_number=get_term_number()
+    sitting_number=get_sitting_number(term_number)
+    voting_number=get_voting_number(term_number,sitting_number)
     print(f"Głosowanie nr {voting_number} na {sitting_number} posiedzeniu {term_number} kadencji Sejmu")
