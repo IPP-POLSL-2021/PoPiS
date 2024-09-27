1b95f1cab73c8ef94720bc56a5b12289446ffa8a
w0jd <dwoitek1@gmail.com>
2024-09-27 13:37:59 +0200

    Model do powidomień

---
+++ Model/RemindersModel.py
@@ -0,0 +1,17 @@
+from sqlalchemy.Types import Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Reminders(Base):
+    __tablename__ = 'reminder'
+
+    id = Column(Integer, primary_key=True)
+    chanelId = Column(Integer, nullable=False)
+
+    committeeSitting = Column(Date, nullable=False)
+    platform = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"<Chanel(id={self.id}, chanelId={self.chanelId}, committeeSitting={self.committeeSitting},platform={self.platform})>"
