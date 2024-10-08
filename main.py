1554ab9b91c4c3735cf74c860c54737c4ccb64e4
w0jd <dwoitek1@gmail.com>
2024-10-08 15:19:52 +0200

    pare błędów o ktróre pthyon bez powodu robił probelmy

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 import asyncio
 from View import test
 
-from Controller.telegrambot import start_telegram_bot
 from View import test2
 st.sidebar.title("Nawigacja")
 
@@ -22,7 +21,3 @@ def ViewSelection():
 
 
 ViewSelection()
-try:
-    start_telegram_bot()
-except Exception as e:
-    print(f"chyba już działa nwm masz tu błąd i naparw{e}")
