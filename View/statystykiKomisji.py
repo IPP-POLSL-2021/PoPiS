62554c8317efacca4db95c266440282641217ab8
w0jd <dwoitek1@gmail.com>
2024-10-14 13:09:42 +0200

    wyswietlanie danych

--- View/statystykiKomisji.py
+++ View/statystykiKomisji.py
@@ -1,5 +1,7 @@
 from Controller.Commitees import ComitteStats, CommiteesList, CommitteeAge
 import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def loadView():
@@ -17,6 +19,26 @@ def loadView():
     # with st.container(height=400):
     #     for MP in MPs:
     #         st.markdown(MP)
+    # wyświetlanie informacji o ilości członkó i ilości komijsi
+    ClubsCount = clubs.apply(lambda x: x.count(), axis=1)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ClubsCount.plot(kind='bar', ax=ax)
+    ax.set_title('Liczba członków w komisjach', fontsize=16)
+
+    ax.set_xlabel('Partie', fontsize=14)
+    ax.set_ylabel('Liczba członków', fontsize=14)
+    plt.xticks(rotation=45, ha='right')
+    st.pyplot(fig)
     st.dataframe(clubs)
-    st.dataframe(MPs)
-    CommitteeAge(clubsButBetter)
+    if selectedCommittee is "łącznie":
+        st.dataframe(MPs)
+    # wykresy wieku
+    DataframeAges = CommitteeAge(clubsButBetter)
+    all_ages = DataframeAges.values.flatten()
+    all_ages = pd.Series(all_ages).dropna()
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.hist(all_ages, bins=10, color='red', edgecolor='black')
+    ax.set_title('Ogólny rozkład wiekowy członków komisji', fontsize=16)
+    ax.set_xlabel('Wiek', fontsize=14)
+    ax.set_ylabel('Liczba członków', fontsize=14)
+    st.pyplot(fig)
