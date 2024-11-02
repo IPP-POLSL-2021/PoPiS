a8328808249748d7704b8dd6676573fe0441171d
w0jd <dwoitek1@gmail.com>
2024-11-02 19:41:08 +0100

    uzyskiwanie wieku posłów wg klubu

--- Controller/MPsStats.py
+++ Controller/MPsStats.py
@@ -1,4 +1,6 @@
 import requests
+from datetime import datetime, timedelta
+import pandas as pd
 
 
 def groupMpsByClub(term):
@@ -9,9 +11,60 @@ def groupMpsByClub(term):
     for Mp in MpsList:
         if Mp["club"] in MpIdByClub:
             MpIdByClub[Mp["club"]].append(Mp["id"])
-            MpNamesByClub[Mp["lastFirstName"]].append(Mp["id"])
+            MpNamesByClub[Mp["club"]].append(Mp["lastFirstName"])
         else:
             MpIdByClub[Mp["club"]] = [Mp["id"]]
-            MpNamesByClub[Mp["lastFirstName"]] = [Mp["id"]]
+            MpNamesByClub[Mp["club"]] = [Mp["lastFirstName"]]
     # print(MpIdByClub)
     return MpIdByClub, MpsList, MpNamesByClub
+
+
+def ageStats(term, MpIdByClub, Mplist):
+    # response = requests.get(f'https://api.sejm.gov.pl/sejm/term{term}/MP')
+    searchedData = 'birthDate'
+    MPs = Mplist
+    current_time = datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
+    if term != 10:
+        termResponse = requests.get(f'https://api.sejm.gov.pl/sejm/term{term}')
+        termInfo = termResponse.json()
+        endOfTerm = termInfo['to']
+        current_time = datetime.strptime(endOfTerm, "%Y-%m-%d")
+    # print(current_time)
+    MPsAge = {}
+    # print(committee)
+    # print(committee.to_dict)
+    # committee = committee.to_dict(orient="list")
+    # print(committee)
+    # searchedData = f'{searchedInfo}'
+    for patry in MpIdByClub:
+        # print(committee[patry])
+        ages = []
+
+        # print(patry)
+        for personId in MpIdByClub[patry]:
+            # print(person)
+            dateOfBirth = [
+                mp['birthDate'] for mp in MPs if mp['id'] == personId]
+            # dateOfBirth = str(Mplist[])
+            # print(datetime.strptime(str(dateOfBirth[0]), '%Y-%m-%d').date())
+            dateOfBirth = str(dateOfBirth).strip("[]'")
+            # print(dateOfBirth)
+            # print("=================")
+
+            ageOfMP = current_time.date() - \
+                datetime.strptime(dateOfBirth, "%Y-%m-%d").date()
+
+            ageOfMP = ageOfMP.days/365
+            # if ageOfMP > maxMPsAge:
+            #     maxMPsAge = ageOfMP
+            # MaxMinMP[0]=
+            ages.append(round(ageOfMP))
+
+        MPsAge[patry] = ages
+        # MPsAge.append()
+        # print(MPsAge)
+    agesDataFrame = pd.DataFrame.from_dict(MPsAge, orient='index')
+
+    # agesDataFrame = pd.DataFrame(MPsAge)
+    print(agesDataFrame)
+    return agesDataFrame, MPsAge
--- View/BasicStats.py
+++ View/BasicStats.py
@@ -4,4 +4,5 @@ from Controller import MPsStats
 
 def loadView():
     st.markdown("hwilowo nic")
-    MPsStats.groupMpsByClub(10)
+    MpGroupedList, MpsList, MpNamesByClub = MPsStats.groupMpsByClub(10)
+    MPsStats.ageStats(10, MpGroupedList, MpsList)
