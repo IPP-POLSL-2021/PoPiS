c8c7a96f22c991ae820cdefddc58801589e3943e
kingbrucelee <90533878+kingbrucelee@users.noreply.github.com>
2024-11-12 11:04:02 +0100

    .

--- api_wrappers/interpelation.py
+++ api_wrappers/interpelation.py
@@ -1,6 +1,8 @@
 import requests
 import datetime
 from api_wrappers.MP import get_name  # Importing get_name from MP module
+from functools import wraps
+
 
 def get_interpelations(term, **params):
     """Returns a list of interpellations with optional filters."""
@@ -18,37 +20,61 @@ def get_reply_body(term, num, key):
     """Returns a reply body in HTML format."""
     return requests.get(f'https://api.sejm.gov.pl/sejm/term{term}/interpellations/{num}/reply/{key}/body').text
 
-def get_title(term, num, response=False):
+# Enable supplying just response or full parameters
+def handle_response(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        response = kwargs.get('response')
+
+        # If response is not provided, try extracting term, sitting, and id from args or kwargs
+        if not response:
+            if len(args) >= 2:
+                term, num = args[:2]  # Positional arguments
+            else:
+                term = kwargs.get('term')
+                num = kwargs.get('num')
+            
+            # Ensure term, num are provided
+            if term is None or num is None:
+                raise ValueError("Provide term and num when response is not given")
+            
+            # Fetch the response
+            response = get_interpelation(term,num)
+            kwargs['response'] = response
+
+        # Call the wrapped function with response
+        return func(*args, **kwargs)
+    
+    return wrapper
+
+@handle_response
+def get_title(term=None, num=None, response=False):
     """Get the title of an interpellation."""
-    if not response:
-        response = get_interpelation(term, num)
     return response.json()['title']
 
-def get_date(term, num, mode, response=False):
+#receipt_date str --> datetime.date: A date when the case was received by Marshall Example: 2020-11-16.
+#sent_date str --> datetime.date: A date when the interpellation was sent from Marshall to recipients Example: 2021-01-20.
+#last_modified str --> datetime.datetime: A date of last modification of a document Example: 2022-09-07 15:01:42.
+@handle_response
+def get_date(term=None, num=None, mode=None, response=False):
     """Get various dates related to an interpellation."""
-    if not response:
-        response = get_interpelation(term, num)
-    data = response.json()
-    
     match mode:
         case 'receipt_date' | 0 | 'data_otrzymania':
-            return datetime.datetime.strptime(data['receiptDate'], '%Y-%m-%d').date()
+            return datetime.datetime.strptime(response.json()['receiptDate'], '%Y-%m-%d').date()
         case 'sent_date' | 1 | 'data_wysłania':
-            return datetime.datetime.strptime(data['sentDate'], '%Y-%m-%d').date()
+            return datetime.datetime.strptime(response.json()['sentDate'], '%Y-%m-%d').date()
         case 'last_modified' | 2 | 'czas_ostatniej_modyfikacji':
-            return datetime.datetime.strptime(data['lastModified'], '%Y-%m-%dT%H:%M:%S').date()
+            return datetime.datetime.strptime(response.json()['lastModified'], '%Y-%m-%dT%H:%M:%S').date()
 
-def get_authors(term, num, response=False):
+@handle_response
+def get_authors(term=None, num=None, response=False):
     """Get the authors of an interpellation."""
-    if not response:
-        response = get_interpelation(term, num)
     IDs = response.json()['from']
     return [get_name(term, id) for id in IDs]
 
-def get_receipent(term, num, response=False):
+@handle_response
+def get_receipent(term=None, num=None, response=False):
     """Get the recipients of an interpellation."""
-    if not response:
-        response = get_interpelation(term, num)
     return response.json()['to']
 
 def get_replies(term, num, response=False):
@@ -75,17 +101,15 @@ def get_replies(term, num, response=False):
     
     return (file_urls, htmls)
 
+@handle_response
 def get_repeated_interpellations(term, num, response=False):
     """Get repeated interpellations (follow-up questions)."""
-    if not response:
-        response = get_interpelation(term, num)
     return response.json().get('repeatedInterpellation', [])
 
 # Additional utility functions
+@handle_response
 def is_repeated_interpellation(term, num, response=False):
     """Check if this is a repeated interpellation."""
-    if not response:
-        response = get_interpelation(term, num)
     return len(response.json().get('repeatedInterpellation', [])) > 0
 
 if __name__ == "__main__":
