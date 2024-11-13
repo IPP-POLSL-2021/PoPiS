ddfc261f7965bb5597969f78ff66850048baa59e
w0jd <dwoitek1@gmail.com>
2024-11-13 19:08:08 +0100

    pełne nazwy komisji

--- View/statystykiKomisji.py
+++ View/statystykiKomisji.py
@@ -7,16 +7,18 @@ from statistics import mean, median, stdev
 from View import _sharedViews
 from st_aggrid import AgGrid
 
+
 def loadView():
     term_number = st.number_input(
         "kadencja sejmu", min_value=1, value=10)
 
-    codes = [committee['code'] for committee in get_committees(term_number)]
+    codes = [
+        f"{committee['name']} - {committee['code']}" for committee in get_committees(term_number)]
     codes.append("łącznie")
     selectedCommittee = st.selectbox(
         "komisja której statystyki cię interesują (niektóre dostępne tylko dla obecnej kadencji)", options=list(codes)
     )
-
+    selectedCommittee = selectedCommittee.split("-")[1][1:]
     committee_stats = get_committee_stats(term_number, selectedCommittee)
     clubs = pd.DataFrame.from_dict(committee_stats['clubs'], orient='index')
     MPs = pd.DataFrame.from_dict(committee_stats['members'], orient='index')
@@ -26,9 +28,9 @@ def loadView():
 
     # Plotly bar chart for ClubsCount
     fig = px.bar(
-        ClubsCount, 
-        x=ClubsCount.index, 
-        y=ClubsCount.values, 
+        ClubsCount,
+        x=ClubsCount.index,
+        y=ClubsCount.values,
         title='Liczba członków w komisjach',
         labels={'x': 'Partie', 'y': 'Liczba członków'}
     )
@@ -42,14 +44,15 @@ def loadView():
     clubs.columns = [str(col).replace('.', '_') for col in clubs.columns]
 
     AgGrid(clubs)
-    #st.dataframe(clubs, use_container_width=True)
-    #if selectedCommittee == "łącznie":
+    # st.dataframe(clubs, use_container_width=True)
+    # if selectedCommittee == "łącznie":
     #    MPs.columns = [str(col).replace('.', '_') for col in MPs.columns]
     #    AgGrid(MPs)
     if selectedCommittee == "łącznie":
         st.dataframe(MPs, use_container_width=True)
 
-    DataframeAges, AgesButDictionary = get_committee_member_ages(clubsButBetter, term_number)
+    DataframeAges, AgesButDictionary = get_committee_member_ages(
+        clubsButBetter, term_number)
     all_ages = DataframeAges.values.flatten()
     all_ages = pd.Series(all_ages).dropna()
 
@@ -61,10 +64,12 @@ def loadView():
         case "wiek":
             _sharedViews.ageGraphs(all_ages, AgesButDictionary, term_number)
         case "edukacja":
-            EducationDictionary = get_committee_member_details(clubsButBetter, term_number, 'edukacja')
+            EducationDictionary = get_committee_member_details(
+                clubsButBetter, term_number, 'edukacja')
             _sharedViews.MoreStats(EducationDictionary)
         case "profesja":
-            EducationDictionary = get_committee_member_details(clubsButBetter, term_number, 'profesja')
+            EducationDictionary = get_committee_member_details(
+                clubsButBetter, term_number, 'profesja')
             _sharedViews.MoreStats(EducationDictionary)
         case "brak":
             st.write("")
