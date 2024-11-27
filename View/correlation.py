a260833740acd8172aeb559484d8719a5f2324fd
w0jd <dwoitek1@gmail.com>
2024-11-27 12:58:53 +0100

    dalsze poprawki wyglądu

--- View/correlation.py
+++ View/correlation.py
@@ -2,6 +2,8 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import json
 from Controller.Results import getResults
+import plotly.express as px
+import plotly.graph_objects as go
 
 
 def loadView():
@@ -13,19 +15,30 @@ def loadView():
     electionSelections = st.selectbox("wybierz poziom administracyjny do analizy ", (
         "województwa", "okręgi", "powiaty", "gminy", "obwody"))
     matrix, Results = getResults(correlationValue, electionSelections, type)
-    st.dataframe(matrix)
+    datafreame_col, polot_col = st.columns([0.6, 0.4])
+    with datafreame_col:
+        st.dataframe(matrix)
+    with polot_col:
+        axisX = st.selectbox(
+            "wybierz pierwszy element korelacji", Results.columns)
+        columns_2 = Results.columns.copy()
+        columns_2 = columns_2.drop(axisX)
+        axisY = st.selectbox("wybierz drugi element korelacji", columns_2)
 
-    axisX = st.selectbox("wybierz pierwszy element korelacji", Results.columns)
-    axisY = st.selectbox("wybierz drugi element korelacji", Results.columns)
+        # fig, ax = plt.subplots()
+        # # st.write(f"korelacja między {axisX}, {axisY}")
+        # ax.scatter(Results[axisX], Results[axisY], color='blue', marker='o')
+        # # Oznaczenie osi i tytuł wykresu
+        # ax.set_xlabel(axisX)
+        # ax.set_ylabel(axisY)
 
-    fig, ax = plt.subplots()
-    st.write(f"korelacja między {axisX, axisY}")
-    ax.scatter(Results[axisX], Results[axisY], color='blue', marker='o')
-    # Oznaczenie osi i tytuł wykresu
-    ax.set_xlabel(axisX)
-    ax.set_ylabel(axisY)
+        # ax.legend()
 
-    ax.legend()
-
-    # Wyświetlenie wykresu w aplikacji
-    st.pyplot(fig)
+        # Wyświetlenie wykresu w aplikacji
+        st.pyplot(fig)
+        values = Results[axisX]
+        fig = px.scatter(Results, x=axisX, y=axisY)
+        # fig.update_layout(
+        #     title=f"korelacja między {axisX}, {axisY}",
+        # )
+        st.plotly_chart(fig)
